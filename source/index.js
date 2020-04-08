368b10ca5b2dbef900f34d1e0cbcc704a603b1fa
Ben Zabloski <ben.zabloski@gmail.com>
2020-04-07 22:11:59 -0400

    Initial commit.

---
+++ source/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
+  app.get("/auth", function (request, response) {
+    const hasuraVariables = {
+      "X-Hasura-Role": "anonymous",
+    };
+
+    response.json(hasuraVariables);
+  });
+
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+}
